d3ae9567b7bfefa9a40cdf1e281efbba2ad7ac2e
Leon Schemberger <l.schemberger@students.htl-leonding.ac.at>
2026-04-02 19:35:01 +0200

    Font wurde hinzugefügt Startseite wurde verbessert HowTo Seite wurde das Grundgerüst gelegt, Erklärung fehlt in der Box Charakter Seite wurde erstellt und fertig gestellt

---
+++ project/script.js
@@ -0,0 +1,9 @@
+function createCharakter() {
+    document.getElementById('start-menu').style.display = 'none';
+
+    document.getElementById('char-creator').style.display = 'flex';
+}
+
+function savePlayer() {
+    document.getElementById('char-creator').style.display = 'none';
+}
