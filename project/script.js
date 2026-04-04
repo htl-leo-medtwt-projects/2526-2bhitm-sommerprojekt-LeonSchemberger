a081974d5ee4bef57611836534023051f06d5763
Leon Schemberger <l.schemberger@students.htl-leonding.ac.at>
2026-04-04 19:26:04 +0200

    Ids und Klassen wurden bei der Charakterseite geändert Career Hub Seite wurde gemacht mit fertigem CSS Rennkalender Seite wurde angefangen

--- project/script.js
+++ project/script.js
@@ -6,4 +6,18 @@ function createCharakter() {
 
 function savePlayer() {
     document.getElementById('char-creator').style.display = 'none';
+
+    document.getElementById('career-hub').style.display = 'flex'
+}
+
+function raceCalendar() {
+    document.getElementById('career-hub').style.display = 'none';
+
+    document.getElementById('race-calendar').style.display = 'flex';
+}
+
+function careerHub() {
+    document.getElementById('race-calendar').style.display = 'none';
+
+    document.getElementById('career-hub').style.display = 'flex'
 }
