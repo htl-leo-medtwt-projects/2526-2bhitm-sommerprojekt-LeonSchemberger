53956a1979bf712810bef4c5c364b5e5d74ca4c1
Leon Schemberger <leon.schemberger@gmail.com>
2026-06-05 19:08:40 +0200

    bilder und rennetscheidung.js wurde hinzugefügt

---
+++ project/json/decision.js
@@ -0,0 +1,58 @@
+const raceEvents = [
+    {
+        id: "auto_quer",
+        image: "AutoQuer.png",
+        radioText: "TEAM RADIO: Gelbe Flagge in Kurve vier! Da steht einer quer auf der Ideallinie! Musst dich sofort entscheiden!",
+        btn1Text: "Kampflinie & Bremsen",
+        btn2Text: "Ausweichen über Kerbs"
+    },
+    {
+        id: "boxenstopp_fehler",
+        image: "BoxenstoppFehler.png",
+        radioText: "TEAM RADIO: Problem beim Reifenwechsel! Das vordere linke Rad klemmt! Sollen wir die sichere Variante wählen und das Gewinde reinigen, oder den Reifen sofort festknallen und das Risiko eingehen?",
+        btn1Text: "Gewinde reinigen (Sicher)",
+        btn2Text: "Sofort festknallen (Risiko)"
+    },
+    {
+        id: "boxen_poker",
+        image: "BoxPoker.png",
+        radioText: "TEAM RADIO: Die Wolken ziehen zu, der Führende geht an die Box! Sollen wir zum Undercut reinkommen oder draußen bleiben?",
+        btn1Text: "Für Undercut reinkommen",
+        btn2Text: "Draußen bleiben"
+    },
+    {
+        id: "bremsduell",
+        image: "Bremsduell.png",
+        radioText: "TEAM RADIO: Du bist im DRS-Fenster! Es gibt eine kleine Lücke auf der Innenseite der nächsten Kurve. Setzt du zum Überholmanöver an?",
+        btn1Text: "Angriff innen setzen!",
+        btn2Text: "Zurückziehen & Abwarten"
+    },
+    {
+        id: "motor_party",
+        image: "Motor.png",
+        radioText: "TEAM RADIO: Wir können für die nächsten zwei Runden den Party-Modus freischalten. Aber Vorsicht, die Motorentemperatur ist kritisch!",
+        btn1Text: "Party-Modus AN!",
+        btn2Text: "Motor schonen"
+    },
+    {
+        id: "platten_schleichend",
+        image: "Platten.png",
+        radioText: "TEAM RADIO: Wir sehen einen schleichenden Plattfuss hinten rechts! Der Druck fällt langsam! Kommst du für einen Sicherheits-Stopp rein oder versuchen wir, das Auto ins Ziel zu retten?",
+        btn1Text: "Sicherheits-Stopp",
+        btn2Text: "Ins Ziel retten"
+    },
+    {
+        id: "regen_wolkenbruch",
+        image: "Regen.png",
+        radioText: "TEAM RADIO: Extremer Wolkenbruch in Sektor zwei! Die Strecke steht unter Wasser! Kommst du für Regenreifen rein oder riskierst du es auf Slicks?",
+        btn1Text: "Inters / Regenreifen",
+        btn2Text: "Auf Slicks riskieren"
+    },
+    {
+        id: "unfall_safetycar",
+        image: "Unfall.png",
+        radioText: "TEAM RADIO: Unfall voraus, das Safety Car kommt raus! Auf der Ideallinie liegen viele Trümmerteile. Boxenstopp einlegen oder draußen bleiben?",
+        btn1Text: "Stopp unter Safety Car",
+        btn2Text: "Draußen bleiben"
+    }
+];
